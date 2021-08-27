40ba1397eea5771bc5362159ca8023aaa0e900ce
DESKTOP-1DQCI56\edr <esm9105@naver.com>
2021-08-27 18:34:48 +0900

    /성민/ 웹 사이트 테스트

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // node_modules 에 있는 express 관련 파일을 가져온다.
 var express = require('express')
 
+var PORT = process.env.PORT
 // express 는 함수이므로, 반환값을 변수에 저장한다.
 var app = express()
 
@@ -10,18 +11,17 @@ app.get('/', function(req,res) {
     res.sendFile(__dirname + "/views/index.html")
 })
 
-// localhost:3000/main 브라우저에 res.sendFile() 내부의 파일이 띄워진다.
-app.get('/main', function(req,res) {
-    res.sendFile(__dirname + "/views/index.html")
-})
+
 
 app.use(express.static('views'))
 
 
 
 // 3000 포트로 서버 오픈
-app.listen(3000, function() {
+app.listen(PORT, function() {
     console.log("start! express server on port 3000")
 })
+
+
 
 // 이제 터미널에 node app.js 를 입력해보자.
